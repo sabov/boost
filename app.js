521e81a7bc348517b69bb3af3cc00d12244502b8
Petr <pereter92@gmail.com>
2013-12-19 15:06:11 +0200

    needed libraries for tunnel added

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Done');
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
