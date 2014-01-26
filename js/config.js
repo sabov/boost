3a97083b4c1acc1ea21fb2f151d324cb1f8b9966
Oleksandr Sabov <alexsabov@gmail.com>
2014-01-26 22:58:20 +0100

    Use config params

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ var config = {
 
     colors: [0xcb3131, 0x338eda, 0xd03ddd],
     radius: 30,
-    tubeLength: 60,
+    tubePieceLength: 200,
     numOfSegments: 12,
-    textureLength: 25,
+    textureLength: 20,
     speed: 0,
     pathLength: 20,
     arrowLength: 8
