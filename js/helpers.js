5d7c5670e9212d08ff68560b01b5d399711b0d56
Oleksandr Sabov <alexsabov@gmail.com>
2014-01-26 21:45:47 +0100

    Align cube in tube

--- js/helpers.js
+++ js/helpers.js
@@ -8,6 +8,10 @@ function angleToRadians(angle) {
     return angle * Math.PI / 180;
 }
 
+function radiansToAngle(radians) {
+    return radians * 180 / Math.PI;
+}
+
 function getDistanceToSegment(numOfSegments, radius) {
     var angle = angleToRadians(360 / numOfSegments);
     return radius * Math.cos(angle/2);
